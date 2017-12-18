938ba0be90c40b8b539d7bc856c90ad5afe9acff
Orestis Kaplanis <orestismail71@gmail.com>
2017-12-18 22:06:36 +0200

    Add files via upload

---
+++ assets/set01.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="set01" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="../../Desktop/mariotileset.png" width="528" height="448"/>
+ <terraintypes>
+  <terrain name="New Terrain" tile="661"/>
+ </terraintypes>
+</tileset>
